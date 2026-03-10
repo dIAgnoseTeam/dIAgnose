3da541db3899695d04635801f667499e423baa25
rubencadalsof-insti <ruben.cadalsofernandez@riberadeltajo.es>
2026-03-10 19:23:22 +0100

    feat: add dynamic filter logic

--- frontend/src/components/ui/FiltersSection.jsx
+++ frontend/src/components/ui/FiltersSection.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 
-const FiltersSection = () => {
-  return (
-    <section className="flex flex-col sm:flex-row justify-end gap-2 mb-6">
-            <div className="flex flex-col gap-1">
-                <label className="text-gray-600 text-xs font-medium">Email del usuario</label>
-                <input type="email" className="text-gray-700 text-sm bg-white border border-gray-200 rounded-md px-4 py-2 focus:outline-none focus:ring-1 focus:ring-emerald-600 focus:border-emerald-600 placeholder:text-gray-700" placeholder="medico@gmail.com"/>
-            </div>
+const FiltersSection = (props) => {
+    const { setFilterUser, setFilterDate, setFilterScore } = props;
 
-            <div className="flex flex-col gap-1">
-                <label className="text-gray-600 text-xs font-medium ">Fecha</label>
-                <input type="date" className="text-gray-700 text-sm bg-white border border-gray-200 rounded-md px-4 py-2 focus:outline-none focus:ring-1 focus:ring-emerald-600 focus:border-emerald-600"/>
-            </div>
+    return (
+        <section className="flex flex-col sm:flex-row justify-end gap-2 mb-6">
+                <div className="flex flex-col gap-1">
+                    <label className="text-gray-600 text-xs font-medium" htmlFor="filter-email">Email del usuario</label>
+                    <input type="email" id="filter-email" className="text-gray-700 text-sm bg-white border border-gray-200 rounded-md px-4 py-2 focus:outline-none focus:ring-1 focus:ring-emerald-600 focus:border-emerald-600 placeholder:text-gray-700" placeholder="medico@gmail.com" onChange={(e) => setFilterUser(e.target.value.trim())}/>
+                </div>
 
-            <div className="flex flex-col gap-1">
-                <label className="text-gray-600 text-xs font-medium ">Puntuación</label>
-                <select className="text-gray-700 text-sm bg-white border border-gray-200 rounded-md px-4 py-2 focus:outline-none focus:ring-1 focus:ring-emerald-600 focus:border-emerald-600">
-                    <option value="">Todas las puntuaciones</option>
-                    <option value="5">5</option>
-                    <option value="4">4</option>
-                    <option value="3">3</option>
-                    <option value="2">2</option>
-                    <option value="1">1</option>
-                </select>
-            </div>
-    </section>
-  )
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="filter-date" className="text-gray-600 text-xs font-medium ">Fecha</label>
+                    <input type="date" id="filter-date" className="text-gray-700 text-sm bg-white border border-gray-200 rounded-md px-4 py-2 focus:outline-none focus:ring-1 focus:ring-emerald-600 focus:border-emerald-600" onChange={(e) => setFilterDate(e.target.value.trim())}/>
+                </div>
+
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="filter-score" className="text-gray-600 text-xs font-medium ">Puntuación</label>
+                    <select id="filter-score" className="text-gray-700 text-sm bg-white border border-gray-200 rounded-md px-4 py-2 focus:outline-none focus:ring-1 focus:ring-emerald-600 focus:border-emerald-600" onChange={(e) => setFilterScore(e.target.value.trim())}>
+                        <option value="">Todas las puntuaciones</option>
+                        <option value="5">5</option>
+                        <option value="4">4</option>
+                        <option value="3">3</option>
+                        <option value="2">2</option>
+                        <option value="1">1</option>
+                    </select>
+                </div>
+        </section>
+    )
 }
 
 export default FiltersSection
